d54dfc6f036937444968bbfc72b6a8612b122149
Luis Carrera <luiscarreradev@gmail.com>
2025-12-10 20:17:55 -0600

    Initial commit: Next.js setup

---
+++ open-next.config.ts
@@ -0,0 +1,9 @@
+import type { OpenNextConfig } from '@opennextjs/cloudflare';
+
+const config: OpenNextConfig = {
+  default: {
+    wrapper: '@opennextjs/cloudflare',
+  },
+};
+
+export default config;
