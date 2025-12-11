45298aa9f5018e4f50b47fb692829f8c498532e9
Luis Carrera <luiscarreradev@gmail.com>
2025-12-10 21:48:43 -0600

    Agregue custom domain a cloudflare

--- .open-next/cloudflare/init.js
+++ .open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1765423781879,
+    __BUILD_TIMESTAMP_MS__: 1765424813846,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
